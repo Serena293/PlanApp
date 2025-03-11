940b5a7e30bc87e025df2a3984308409e20a24d8
SERENA FERRARO <serenaferraro21@gmail.com>
2025-03-11 11:42:03 +0000

    Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-// import './App.css'
-import HomePage from "./components/HomePage";
-// import LoginPage from "./components/LoginPage";
+import { useState, useEffect } from 'react';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+ 
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    if (storedLoginStatus === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true'); 
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.setItem('isLoggedIn', 'false'); 
+  };
+
   return (
     <div className="d-flex justify-content-around w-100 align-items-center vh-100">
-  {/* <LoginPage/> */}
-  <HomePage/>
+      {isLoggedIn ? (
+        <HomePage onLogout={handleLogout} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
